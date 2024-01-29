d71f0ca0be4c41d80f92ca7e1a17394018926ac6
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2024-01-29 21:01:59 +0900

    [Bronze V] Title: 구구단, Time: 156 ms, Memory: 16128 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,17 +1,15 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class Main{
-    public static void main(String[] args){
+    public static void main(String[] args)throws IOException{
     
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n;
+        int n = Integer.parseInt(br.readLine());
 
-        n = sc.nextInt();
-        
         for(int i=1;i<=9;i++){
-            System.out.println(n + " * " + i + " = " + n*i);
+            System.out.println(n + " * "+ i + " = " + n*i);
         }
-        sc.close();
-
     }
 }
