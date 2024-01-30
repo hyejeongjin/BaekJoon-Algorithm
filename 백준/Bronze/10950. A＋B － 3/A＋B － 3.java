d7d2f8cf7ac8e30e1c174f9474725912526cb5f7
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2024-01-30 21:08:25 +0900

    [Bronze V] Title: A+B - 3, Time: 228 ms, Memory: 17952 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+
+        int t;
+
+        t = sc.nextInt();
+
+        int[] a = new int[t];
+        int[] b = new int[t];
+
+
+        for(int i=0; i<t; i++){
+            a[i] = sc.nextInt();
+            b[i] = sc.nextInt();
+
+        }
+        for(int i=0; i<t; i++){
+            System.out.println(a[i] + b[i]);
+        }
+            sc.close();
+    }
+}
