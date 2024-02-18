15ef1bd808d5bc0deca12e72eb15f4ac149db0a9
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2024-02-18 13:40:42 +0900

    [Bronze II] Title: 나머지, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,23 +1,22 @@
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
 
 public class Main{
-    public static void main(String[] args){
+    public static void main(String[] args)throws IOException{
 
-        Scanner sc = new Scanner(System.in);
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         Integer[] num = new Integer[10];
-        Set<Integer> set = new HashSet<Integer>();
- 
-        for(int i=0; i<=9; i++){
-            num[i] = sc.nextInt();
-            set.add(num[i] % 42);
+        HashSet<Integer> set = new HashSet<Integer>();
 
+        for(int i=0; i<num.length; i++){
+            num[i] = Integer.parseInt(br.readLine());
+            set.add(num[i] % 42);
         }
         System.out.println(set.size());
-       
-        sc.close();
+
+        br.close();
     }
 }
