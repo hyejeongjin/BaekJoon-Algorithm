9b136cb80eac3890b49782c98ee0b7aa90f0bb81
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2024-02-22 18:40:43 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 204 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        //영어단어
+        String S = sc.nextLine();
+
+        System.out.println(S.length());
+         
+        sc.close();
+    }
+}
