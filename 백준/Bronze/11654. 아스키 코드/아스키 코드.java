3961d96216bbcfde95e8131e46e12793052a3c29
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2024-02-23 22:02:55 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -1,14 +1,16 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main{
-    public static void main(String[] args){
+    public static void main(String[] args)throws IOException{
 
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        char input = sc.next().charAt(0);
+        char input = br.readLine().charAt(0);
 
         System.out.println((int)input);
          
-        sc.close();
+        br.close();
     }
 }
