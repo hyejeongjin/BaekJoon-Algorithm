52cddd4864f0f41d204605175af5c6fa25735938
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2024-02-27 21:15:41 +0900

    [Bronze II] Title: 단어의 개수, Time: 252 ms, Memory: 20012 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,15 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main{
-public static void main(String[] args){
+public static void main(String[] args)throws IOException{
 
-    Scanner sc = new Scanner(System.in);
-    String sen = sc.nextLine();
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String sen = br.readLine();
 
     StringTokenizer st = new StringTokenizer(sen, " ");
 
     System.out.println(st.countTokens());
-        sc.close();
+        br.close();
     }
 }
