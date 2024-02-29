59dcad0d12b8d0d63c93d55a7b060a0fa114a174
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2024-02-29 22:08:58 +0900

    [Bronze II] Title: 다이얼, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -1,11 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main{
-public static void main(String[] args){
+public static void main(String[] args)throws IOException{
 
-    Scanner sc = new Scanner(System.in);
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    String word = sc.next();
+    String word = br.readLine();
     int sum = 0;
 
     for(int i=0; i<word.length(); i++){
@@ -44,6 +47,6 @@ public static void main(String[] args){
         }
     }
     System.out.println(sum);
-    sc.close();
+    br.close();
     }
 }
