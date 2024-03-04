a6d73e236576b8e3b2d54bde3ba46beeed84617d
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2024-03-04 15:48:28 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 144 ms, Memory: 15836 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -1,22 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main{
-public static void main(String[] args){
+public static void main(String[] args)throws IOException{
 
-Scanner sc = new Scanner(System.in);
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-int[] list = {1, 1, 2, 2, 2, 8};
+    int[] list = {1, 1, 2, 2, 2, 8};
 
-int[] input = new int[6];
+    int[] input = new int[6];
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    for(int i=0; i<6; i++){
+        input[i] = Integer.parseInt(st.nextToken());
+    }
 
-for(int i=0; i< 6; i++){
-    input[i] = sc.nextInt();
-}
+    for(int i=0; i<6; i++){
+        list[i] -= input[i];
+        System.out.print(list[i] + " ");
 
-for(int i=0; i<6; i++){
-    list[i] -= input[i];
-    System.out.print(list[i] + " ");
+    }
 
-}
+    br.close();
+
     }
 }
