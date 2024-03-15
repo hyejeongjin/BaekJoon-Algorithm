5bf4c7af9e54f1ba744b63d1dab9c25df65f3162
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2024-03-15 20:37:29 +0900

    [Bronze III] Title: 최댓값, Time: 148 ms, Memory: 15952 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,9 +1,11 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 public class Main{
-public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
+public static void main(String[] args)throws IOException{
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     int[][] A = new int[9][9];
     int max = -1;
@@ -11,8 +13,9 @@ public static void main(String[] args){
     int y = 0;
     
     for(int i=0; i<9; i++){
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for(int j=0; j<9; j++){
-            A[i][j] = sc.nextInt();
+            A[i][j] = Integer.parseInt(st.nextToken());
         }
     }
     for(int i=0; i<9; i++){
@@ -27,6 +30,6 @@ public static void main(String[] args){
     System.out.println(max);
     System.out.println(x + " " + y);
     
-    sc.close();
+    br.close();
     }
 }
