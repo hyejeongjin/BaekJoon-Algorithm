65958f96dcb8840c1a9c665efe64aa7af6c7c806
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2024-03-19 00:33:20 +0900

    [Silver V] Title: 색종이, Time: 124 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,17 +1,21 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 public class Main{
-public static void main(String[] args){
+public static void main(String[] args)throws IOException{
 
-    Scanner sc = new Scanner(System.in);
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     boolean[][] sq = new boolean[100][100];
     
-    int num = sc.nextInt();
+    int num = Integer.parseInt(br.readLine());
     int x=0, y=0, sum=0;
 
     for(int i=0; i<num; i++){
-        x = sc.nextInt();
-        y = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        x = Integer.parseInt(st.nextToken());
+        y = Integer.parseInt(st.nextToken());
 
         for(int j=x; j<x+10; j++){
             for(int k=y; k<y+10; k++){
@@ -23,6 +27,6 @@ public static void main(String[] args){
         }
     }
     System.out.println(sum);
-    sc.close();
+    br.close();
     }
 }
