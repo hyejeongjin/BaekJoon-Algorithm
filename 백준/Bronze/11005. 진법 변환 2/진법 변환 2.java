44835f2132bcae4b6782c500683cd1c726f24ef0
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2024-03-20 00:32:05 +0900

    [Bronze I] Title: 진법 변환 2, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -1,15 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main{
-public static void main(String[] args){
+public static void main(String[] args)throws IOException{
 
-    Scanner sc = new Scanner(System.in);
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-    int N = sc.nextInt();
-    int B = sc.nextInt();
+    int N = Integer.parseInt(st.nextToken());
+    int B = Integer.parseInt(st.nextToken());
 
     System.out.println(Integer.toString(N, B).toUpperCase());
 
-    sc.close();
+    br.close();
     }
 }
