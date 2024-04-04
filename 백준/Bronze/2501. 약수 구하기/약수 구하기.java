52014679cf5bb1a336b73e2f0cbe68a3484b0d65
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2024-04-04 23:29:08 +0900

    [Bronze III] Title: 약수 구하기, Time: 128 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.java
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -1,15 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main{
-public static void main(String[] args){
+public static void main(String[] args)throws IOException{
 
-    Scanner sc = new Scanner(System.in);
-    
-    int N = sc.nextInt();
-    int K = sc.nextInt();
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+    int N = Integer.parseInt(st.nextToken());
+    int K = Integer.parseInt(st.nextToken());
     List<Integer> num = new ArrayList<Integer>();
 
     for(int i=1; i<=N; i++){
@@ -24,6 +29,6 @@ public static void main(String[] args){
     else{
         System.out.println(num.get(K-1));
     }
-    sc.close();
+    br.close();
     }
 }
