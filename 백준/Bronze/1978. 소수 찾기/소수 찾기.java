015179f45a2e3b7ab701f230994d96e1172d01e7
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2024-04-05 23:38:46 +0900

    [Bronze II] Title: 소수 찾기, Time: 128 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,15 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main{
-public static void main(String[] args){
+public static void main(String[] args)throws IOException{
 
-    Scanner sc = new Scanner(System.in);
-    int N = sc.nextInt();
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
     int num = 0;
     int count = 0;
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
     for(int i=0; i<N; i++){
-        num = sc.nextInt();
-
+        num = Integer.parseInt(st.nextToken());
         for(int j=2; j<=num; j++){
             if(j == num){
                 count++;
@@ -20,7 +23,6 @@ public static void main(String[] args){
         }
     }
     System.out.print(count);
-
-    sc.close();
+    br.close();
     }
 }
