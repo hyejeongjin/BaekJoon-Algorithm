3030d962ff82cd9a5d238f6f1e256dff93e8c0b8
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2024-04-09 21:50:08 +0900

    [Bronze V] Title: 직사각형, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/27323. 직사각형/직사각형.java
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -1,13 +1,15 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main{
-public static void main(String[] args){
+public static void main(String[] args)throws IOException{
 
-    Scanner sc = new Scanner(System.in);
-    int A = sc.nextInt();
-    int B = sc.nextInt();
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int A = Integer.parseInt(br.readLine());
+    int B = Integer.parseInt(br.readLine());
 
     System.out.println(A*B);
-    sc.close();
+    br.close();
     }
 }
