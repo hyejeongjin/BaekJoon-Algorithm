e01460d59b26b35e7529e51bb01c0d8fdd600da9
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2024-04-12 23:42:56 +0900

    [Bronze III] Title: 대지, Time: 284 ms, Memory: 27224 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.java
+++ 백준/Bronze/9063. 대지/대지.java
@@ -1,27 +1,30 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main{
-public static void main(String[] args){
+public static void main(String[] args)throws IOException{
     
-    Scanner sc = new Scanner(System.in);
-    int N = sc.nextInt();
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
     int xmax = -10000;
     int xmin = 10000;
     int ymax = -10000;
     int ymin = 10000;
 
     for(int i=0; i<N; i++){
-        int x = sc.nextInt();
-        int y = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
 
         xmax = Math.max(xmax, x);
         xmin = Math.min(xmin, x);
         ymax = Math.max(ymax, y);
         ymin = Math.min(ymin, y);
     }
-
     System.out.println((xmax-xmin)*(ymax-ymin));
-
-    sc.close();
+    br.close();
     }
 }
