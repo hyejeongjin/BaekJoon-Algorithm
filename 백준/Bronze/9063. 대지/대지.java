c3bcef4d8d988e110ab6cb63f462a797815a7df4
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2024-04-12 23:40:59 +0900

    [Bronze III] Title: 대지, Time: 988 ms, Memory: 101084 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main{
+public static void main(String[] args){
+    
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int xmax = -10000;
+    int xmin = 10000;
+    int ymax = -10000;
+    int ymin = 10000;
+
+    for(int i=0; i<N; i++){
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        xmax = Math.max(xmax, x);
+        xmin = Math.min(xmin, x);
+        ymax = Math.max(ymax, y);
+        ymin = Math.min(ymin, y);
+    }
+
+    System.out.println((xmax-xmin)*(ymax-ymin));
+
+    sc.close();
+    }
+}
