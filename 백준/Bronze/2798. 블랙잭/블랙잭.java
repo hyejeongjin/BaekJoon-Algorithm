d488c2e3efcfdb95c6a240849d7c5a608352f6fb
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2024-04-24 22:28:34 +0900

    [Bronze II] Title: 블랙잭, Time: 128 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,18 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main{
-public static void main(String[] args){
+public static void main(String[] args)throws IOException{
     
-    Scanner sc = new Scanner(System.in);
-
-    int N = sc.nextInt();
-    int M = sc.nextInt();
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
     int[] list = new int[N];
     int sum = 0;
     int check = 0;
 
+    st = new StringTokenizer(br.readLine(), " ");
     for(int i=0; i<N; i++){
-        list[i] = sc.nextInt();
+        list[i] = Integer.parseInt(st.nextToken());
     }
 
     for(int i=0; i<N-2; i++){
@@ -26,6 +31,6 @@ public static void main(String[] args){
         }
     }
     System.out.println(check);
-    sc.close();
+    br.close();
     }
 }
