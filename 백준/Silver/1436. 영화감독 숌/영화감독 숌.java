e5f5783f8467992a5ec3d50fb72e8d51f3e12cd7
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2024-05-14 00:05:00 +0900

    [Silver V] Title: 영화감독 숌, Time: 304 ms, Memory: 88416 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,9 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main{
-public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    int N = sc.nextInt();
+public static void main(String[] args)throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
 
     int num = 666;
     int cnt = 1;
@@ -16,6 +19,6 @@ public static void main(String[] args){
     }
     System.out.println(num);
 
-    sc.close();
+    br.close();
     }
 }
