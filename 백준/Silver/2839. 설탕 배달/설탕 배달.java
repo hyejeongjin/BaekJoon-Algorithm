3cf2bb677403394a92157029df331a08bd928d01
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2024-05-28 18:34:09 +0900

    [Silver IV] Title: 설탕 배달, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,9 +1,11 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main{
-public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    int N = sc.nextInt();
+public static void main(String[] args)throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
     int bag = 0;
 
     while(N>0){
@@ -19,9 +21,7 @@ public static void main(String[] args){
             break;
         }  
     }
-
     System.out.println(bag);
-    sc.close();
-
+    br.close();
     }
 }
