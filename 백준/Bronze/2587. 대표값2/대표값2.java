61faa7e7f41e109451b76473cb4064bf2367ec4a
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2024-05-30 00:29:25 +0900

    [Bronze II] Title: 대표값2, Time: 120 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.java
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -1,16 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main{
-public static void main(String[] args){
+public static void main(String[] args)throws IOException{
 
-    Scanner sc = new Scanner(System.in);
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int[] listArr = new int[5];
     int sum = 0;
     int avg = 0;
 
     for(int i=0; i<listArr.length; i++){
-        listArr[i] = sc.nextInt();
+        listArr[i] = Integer.parseInt(br.readLine());
     }
     
     Arrays.sort(listArr);
@@ -20,6 +22,6 @@ public static void main(String[] args){
     }
     System.out.println(avg);
     System.out.println(listArr[2]);
-    sc.close();
+    br.close();
     }
 }
