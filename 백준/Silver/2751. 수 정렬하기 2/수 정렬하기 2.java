31b6535a8b418f496542ab204c2aefff3970b1c2
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2024-05-31 22:58:31 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 2772 ms, Memory: 334608 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main{
+public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    StringBuilder sb = new StringBuilder();
+    int N = sc.nextInt();
+    List<Integer> list = new ArrayList<>();
+    
+    for(int i=0; i<N; i++){
+        list.add(sc.nextInt());
+    }
+    Collections.sort(list);
+    for(int i: list){
+        sb.append(i).append('\n');
+    }
+    System.out.println(sb);
+    sc.close();
+    }
+}
