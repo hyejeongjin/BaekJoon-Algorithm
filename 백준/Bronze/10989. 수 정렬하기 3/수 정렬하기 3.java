841ffcd51973c4235faa0a18fdbaf5bc5a808c8b
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2024-06-05 21:17:10 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1676 ms, Memory: 337084 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,22 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-
 public class Main{
 public static void main(String[] args)throws IOException{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringBuilder sb = new StringBuilder();
    
     int N = Integer.parseInt(br.readLine());
-    int[] list = new int[N];
+    //수의 범위가 1~10000
+    int[] range = new int[10001];
     
+    //해당 값을 인덱스로 하는 배열 값 증가
     for(int i=0; i<N; i++){
-        list[i] = Integer.parseInt(br.readLine());
+        range[Integer.parseInt(br.readLine())]++;
     }
-    Arrays.sort(list);
-    for(int i: list){
-        sb.append(i).append("\n");
+
+    for(int i = 1; i < 10001; i++){
+        // i 값이 개수가 0 이 될 때 까지 출력 (빈도수를 의미)
+        while(range[i] > 0){
+            sb.append(i).append('\n');
+            range[i]--;
+        }
     }
     System.out.println(sb);
     br.close();
