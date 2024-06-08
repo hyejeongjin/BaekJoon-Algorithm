cecbb36e6f07002a880c66a84fe88901f14978f0
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2024-06-09 00:46:41 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 808 ms, Memory: 60364 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -18,16 +18,13 @@ public static void main(String[] args)throws IOException{
         arr[i][1] = Integer.parseInt(st.nextToken());
     }
 
-    Arrays.sort(arr, new Comparator<int[]>() {
-        @Override
-        public int compare(int[]e1, int[] e2){
-            if(e1[0] == e2[0]) {		// 첫번째 원소가 같다면 두 번째 원소끼리 비교
-                return e1[1] - e2[1];
-            }
-            else {
-                return e1[0] - e2[0];
-            }
-        }    
+    Arrays.sort(arr, (e1, e2)->{
+        if(e1[0] == e2[0]){
+            return e1[1] - e2[1];
+        }else{
+            return e1[0] - e2[0];
+        }
+
     });
     
     for(int i=0; i<N; i++){
