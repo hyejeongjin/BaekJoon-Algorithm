3ba779396169c2a27cabc97fc47b45c534a41944
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2024-06-09 00:41:11 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 844 ms, Memory: 60624 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main{
+public static void main(String[] args)throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    int N = Integer.parseInt(br.readLine());
+    int[][] arr = new int[N][2];
+
+    for(int i=0; i<N; i++){
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        arr[i][0] = Integer.parseInt(st.nextToken());
+        arr[i][1] = Integer.parseInt(st.nextToken());
+    }
+
+    Arrays.sort(arr, new Comparator<int[]>() {
+        @Override
+        public int compare(int[]e1, int[] e2){
+            if(e1[0] == e2[0]) {		// 첫번째 원소가 같다면 두 번째 원소끼리 비교
+                return e1[1] - e2[1];
+            }
+            else {
+                return e1[0] - e2[0];
+            }
+        }    
+    });
+    
+    for(int i=0; i<N; i++){
+            sb.append(arr[i][0] + " ");
+            sb.append(arr[i][1] + "\n");
+    }
+    System.out.println(sb);
+}
+}
+
