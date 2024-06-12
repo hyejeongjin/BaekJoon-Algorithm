fa20ba0acb9fc8ba548525a08fb1459b49ad6cd2
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2024-06-13 01:10:08 +0900

    [Silver V] Title: 단어 정렬, Time: 3208 ms, Memory: 52376 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,36 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Main{
-public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    int N = sc.nextInt();
+public static void main(String[] args)throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
     List<String> list = new ArrayList<>();
 
     for(int i=0; i<N; i++){
-        list.add(sc.next());
+        list.add(br.readLine());
     }
-
     //1.단어 중복 제거
     list = list.stream().distinct().collect(Collectors.toList());
-    String[] arr = list.toArray(new String[list.size()]);
-    
-    //2.길이가 짧은 것부터 정렬
-    Arrays.sort(arr);
-    //3.길이가 같으면 사전 순으로
+    String[] arr = list.toArray(new String[list.size()]);  
+    //2.사전순으로 정렬
+    Arrays.sort(arr);   
+    //3.길이 비교
     for(int i=0; i<arr.length; i++){
-        Arrays.sort(arr, (String s1, String s2) -> s1.length() - s2.length());
-       
+        Arrays.sort(arr, (String s1, String s2) -> s1.length() - s2.length());      
     }
-
     for(int i=0; i<arr.length; i++){
         System.out.println(arr[i]);
     }
-
-sc.close();
+br.close();
 }
 }
